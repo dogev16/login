449d69a3d9e49ab3e473a30fbf7c7da9ecab0d4b
dogev16 <148747229+dogev16@users.noreply.github.com>
2025-05-13 15:18:26 +0800

    Update settings.py

--- login/settings.py
+++ login/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yos=ep8ck)$2y872cgkn&o-7mzbg=ey1ja-gl6(j7mgdebi!s6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'login.urls'
